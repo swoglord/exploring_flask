155849f297954343e8a2c25311fd3dd5c78d200d
Timothy Leow <timothyleow12@gmail.com>
2021-07-25 18:36:19 +0800

    Added main features of the app, adding and deleting links and viewing one's history. also developed some trash management feature. Working on editing link data feature and optimising everything else!

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
+from datetime import datetime
+from tempfile import mkdtemp
 from flask import Flask, redirect, render_template, session, request, flash
+from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import apology
+from werkzeug.exceptions import (HTTPException, InternalServerError,
+                                 default_exceptions)
+from helpers import apology, login_required
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key='test'
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 #configure database connected to mysql (used phpmyadmin)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/linklearn'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,9 +41,15 @@ class users(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.name
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+class links(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    username = db.Column("username", db.Text, nullable=False)
+    url = db.Column("url", db.Text, nullable=False)
+    nickname = db.Column("nickname", db.Text, nullable=False)
+    description = db.Column("description", db.Text, nullable=False)
+    timestamp = db.Column("timestamp", db.TIMESTAMP, nullable=False)
+    deleted = db.Column("deleted", db.Boolean, default=False, nullable=False)
+
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -50,7 +78,7 @@ def login():
 
         # Remember which user has logged in
         session["user_id"] = user.id
-
+        session["username"] = user.username
         # Redirect user to home page
         flash("Log-in successful!")
         return redirect("/")
@@ -58,16 +86,6 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    """Log user out"""
-
-    # Forget any user_id
-    session.clear()
-
-    # Redirect user to login form
-    return redirect("/")
 
 @app.route('/register', methods=["GET","POST"])
 def register():
@@ -103,3 +121,89 @@ def register():
     else:
         #this handles GET requests. condition "else " is used because GET is the only other request method other than POST here.
         return render_template("register.html")
+
+@app.route('/', methods=["GET", "POST"])
+@login_required
+def index():
+    if request.method == "POST":
+        if request.form.get("delete"):
+            link_id = request.form.get("link_id")
+            link_to_delete = links.query.filter_by(id=link_id).first()
+            link_to_delete.deleted = True
+            db.session.commit()
+            flash("You deleted the link, {link}! It's now in your trash and you can restore it or permanently delete it as you wish.".format(link=link_to_delete.nickname))
+            return redirect('/trash')
+    user_links = links.query.filter_by(username=session["username"]).all()
+    return render_template("index.html", user_links=user_links)
+
+
+@app.route('/add', methods=["GET", "POST"])
+@login_required
+def add():
+    if request.method == "POST":
+        # error checking - figure out how to check if a url is a valid link - learn about regex!
+        #TODO
+        username = session["username"]
+        url = request.form.get("url")
+        nickname = request.form.get("nickname")
+        description = request.form.get("description")
+        if not url or not nickname or not description:
+            return apology("You must fill in all fields to add in a link!")
+        #if nothing wrong, add this link to the table, links!
+        timestamp = datetime.now()
+        link = links(username=username, url=url, nickname=nickname, description=description, timestamp=timestamp)
+        db.session.add(link)
+        db.session.commit()
+        flash("You added a new link, {nickname}!".format(nickname=link.nickname))
+        return redirect("/")
+    return render_template("add.html")
+
+@app.route("/history")
+@login_required
+def history():
+    user_links = links.query.filter_by(username=session["username"]).all()
+    return render_template("history.html", user_links=user_links)
+
+@app.route("/trash", methods=['GET','POST'])
+@login_required
+def trash():
+    if request.method == "POST":
+        if request.form.get("restore"):
+            restore_id = request.form.get("restore_id")
+            link_to_restore = links.query.filter_by(id=restore_id).first()
+            link_to_restore.deleted = False
+            db.session.commit()
+            flash("You restored the link, {link}!".format(link=link_to_restore.nickname))
+            return redirect('/')
+        elif request.form.get("delete"):
+            delete_id = request.form.get("delete_id")
+            link_to_delete = links.query.filter_by(id=delete_id).first()
+            nickname = link_to_delete.nickname
+            db.session.delete(link_to_delete)
+            db.session.commit()
+            flash("You permanently deleted the link, {nickname}!".format(nickname=nickname))
+            return redirect('/trash')
+    user_links = links.query.filter_by(username=session["username"]).all()
+    return render_template("trash.html", user_links=user_links)
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
