8c7e43073b036577fb6b3e5845d7b04e4d2f12f0
Timothy Leow <you@example.com>
2021-07-30 14:16:38 +0800

    removed commands.py as can just run interactive python shell in heroku bash terminal. Also reconfigured Procfile as previous commit caused app error

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn -w 1 :app --preload
+web: gunicorn app:app --preload
+
--- commands.py
+++
@@ -1,9 +0,0 @@
-import click 
-from flask.cli import with_appcontext
-
-from app import db, users, links
-
-@click.command(name='create_tables')
-@with_appcontext
-def create_tables():
-    db.create_all()
